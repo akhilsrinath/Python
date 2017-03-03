6bac0b0584f05070c5bb556ea137169634895fc2
Akhil Srinath <akhilcr9@gmail.com>
2017-03-03 21:08:51 +0530

    Update integrate.py

--- primer/integrate.py
+++ primer/integrate.py
@@ -3,9 +3,9 @@
 import numpy as np
 
 def integrate(f, a, b, N):
-    x = np.linspace(a+(b-a)/(2*N), b-(b-a)/(2*N), N)
+    x = np.linspace(a+(b-a)/(2*N), b-(b-a)/(2*N), N)    # a number line with N points between a and b stored in a vector x
     fx = f(x)
-    area = np.sum(fx)*(b-a)/N
+    area = np.sum(fx)*(b-a)/N    # total area of all the rectangles combined
     return area
 
 print(integrate(np.sin, 0, np.pi/2, 100))
