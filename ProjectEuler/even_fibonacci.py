e13b2ca86b0bb5051c72e4f1817c6666ce1e1a68
Akhil Srinath <akhilcr9@gmail.com>
2017-03-14 11:01:40 +0530

    Update even_fibonacci.py

--- ProjectEuler/even_fibonacci.py
+++ ProjectEuler/even_fibonacci.py
@@ -12,7 +12,7 @@ def even_fib_sum():
     a,b = 0,1
     total = 0
     while a<4000000:
-        a,b = b,a+b
+        a,b = b,a+b     # a = b, b = a+b
         if a%2 == 0:
             total+=a
     print(total)
