edba1a2793f5f253034780520d4807f5be155587
Akhil Srinath <akhilcr9@gmail.com>
2018-06-04 09:08:31 +0530

    Add files via upload

--- linear_regression.py
+++ linear_regression.py
@@ -1,22 +1,11 @@
-
-# Simple Linear Regression
-
-The goal of this project was to build a linear regression model from the ground up using numpy.
-
-
-```python
-%matplotlib inline
-
-#imports
-from numpy import *
+import numpy as np
 import matplotlib.pyplot as plt
-```
-
-#### Import the data
-Here, we're using a dataset with two columns containing the amount of hours studied and the test scores students achieved, respectively.
+from numpy import *
 
 
-```python
+# import the data
+
+
 points = genfromtxt('data.csv', delimiter=',')
 
 #Extract columns
@@ -29,45 +18,32 @@ plt.xlabel('Hours of study')
 plt.ylabel('Test scores')
 plt.title('Dataset')
 plt.show()
-```
 
+# Defining the hyperparameters
 
-![png](linear-regression-demo_files/linear-regression-demo_3_0.png)
-
-
-#### Defining the hyperparamters
-
-
-```python
 #hyperparamters
 learning_rate = 0.0001
 initial_b = 0
 initial_m = 0
 num_iterations = 10
-```
 
-#### Define cost function
+# Defining the Cost Function
 
-
-```python
 def compute_cost(b, m, points):
     total_cost = 0
     N = float(len(points))
-    
+
     #Compute sum of squared errors
     for i in range(0, len(points)):
         x = points[i, 0]
         y = points[i, 1]
         total_cost += (y - (m * x + b)) ** 2
-        
+
     #Return average of squared error
     return total_cost/N
-```
 
-#### Define Gradient Descent functions
+# Defining Gradient Descent functions
 
-
-```python
 def gradient_descent_runner(points, starting_b, starting_m, learning_rate, num_iterations):
     b = starting_b
     m = starting_m
@@ -91,19 +67,16 @@ def step_gradient(b_current, m_current, points, learning_rate):
         y = points[i, 1]
         m_gradient += - (2/N) * x * (y - (m_current * x + b_current))
         b_gradient += - (2/N) * (y - (m_current * x + b_current))
-    
+
     #Update current m and b
     m_updated = m_current - learning_rate * m_gradient
     b_updated = b_current - learning_rate * b_gradient
 
     #Return updated parameters
     return b_updated, m_updated
-```
 
-#### Run gradient_descent_runner() to get optimized parameters b and m
+# Run gradient_descent_runner() to get optimized parameters b and m
 
-
-```python
 b, m, cost_graph = gradient_descent_runner(points, initial_b, initial_m, learning_rate, num_iterations)
 
 #Print optimized parameters
@@ -112,34 +85,17 @@ print ('Optimized m:', m)
 
 #Print error with optimized parameters
 print ('Minimized cost:', compute_cost(b, m, points))
-```
 
-    Optimized b: 0.0296393478747
-    Optimized m: 1.47741737555
-    Minimized cost: 112.655851815
+#Plotting the cost per iterations
 
-
-#### Plotting the cost per iterations
-
-
-```python
 plt.plot(cost_graph)
 plt.xlabel('No. of iterations')
 plt.ylabel('Cost')
 plt.title('Cost per iteration')
 plt.show()
-```
 
+# Plot line of best fit
 
-![png](linear-regression-demo_files/linear-regression-demo_13_0.png)
-
-
-Gradient descent converges to local minimum after 5 iterations
-
-#### Plot line of best fit
-
-
-```python
 #Plot dataset
 plt.scatter(x, y)
 #Predict y values
@@ -150,8 +106,3 @@ plt.xlabel('Hours of study')
 plt.ylabel('Test scores')
 plt.title('Line of best fit')
 plt.show()
-```
-
-
-![png](linear-regression-demo_files/linear-regression-demo_16_0.png)
-
